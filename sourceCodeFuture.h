e907cc23450602aa478a1b2122c3534f4b1e5a18
BertramFZJ <s.sukov.jsc@gmail.com>
2025-10-15 15:53:07 +0200

    Added macros to header files

--- sourceCodeFuture.h
+++ sourceCodeFuture.h
@@ -1,2 +1,7 @@
+#ifndef _HEADER_FUTURE_
+#define _HEADER_FUTURE_
+
 void scSubroutineMult2(int &val);
 void scSubroutineDev2(int &val);
+
+#endif
--- sourceCodeMaster.h
+++ sourceCodeMaster.h
@@ -1,3 +1,7 @@
-void scSubroutinePlus1(int &val);
+#ifndef _HEADER_MASTER_
+#define _HEADER_MASTER_
 
+void scSubroutinePlus1(int &val);
 void scSubroutineMinus1(int &val);
+
+#endif
